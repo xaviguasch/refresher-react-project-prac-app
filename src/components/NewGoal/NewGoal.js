9e1a06128a89d783ddc95cdfc28eccbbcd9ee2d5
xaviguasch <xaviguasch@gmail.com>
2020-03-02 20:27:07 +0100

    fetched user input in NewGoal and updated and rerendered the course goals

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './NewGoal.css'
 
 function NewGoal(props) {
+  const [enteredText, setEnteredText] = useState('')
+
   const addGoalHandler = e => {
     e.preventDefault()
 
     const newGoal = {
       id: Math.random().toString(),
-      text: 'My new goal'
+      text: enteredText
     }
+
+    setEnteredText('')
 
     props.onAddGoal(newGoal)
   }
 
+  const textChangeHandler = e => {
+    setEnteredText(e.target.value)
+  }
+
   return (
     <form className='new-goal' onSubmit={addGoalHandler}>
-      <input type='text' />
+      <input type='text' value={enteredText} onChange={textChangeHandler} />
       <button type='submit'>Add Goals</button>
     </form>
   )
