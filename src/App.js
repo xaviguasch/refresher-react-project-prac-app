43cbb9a578f1508ac6e8f7f6a65b20759b817da4
xaviguasch <xaviguasch@gmail.com>
2020-03-02 19:15:57 +0100

    added state with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import GoalList from './components/GoalList/GoalList'
 import NewGoal from './components/NewGoal/NewGoal'
 import './App.css'
 
 const App = () => {
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     {
       id: 'cg1',
       text: 'Finish the course'
@@ -18,11 +18,10 @@ const App = () => {
       id: 'cg3',
       text: 'Help others'
     }
-  ]
+  ])
 
   const addNewGoalHandler = newGoal => {
-    courseGoals.push(newGoal)
-    console.log(courseGoals)
+    setCourseGoals(courseGoals.concat(newGoal))
   }
 
   return (
